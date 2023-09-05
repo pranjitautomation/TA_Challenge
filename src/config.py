17b44ac131af2cc549628ec09c93989917f86497
suyash <suyash.dhir@thoughtful.ai>
2023-09-05 14:47:23 +0530

    new master branch

--- src/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dataclasses import dataclass
-
-
-@dataclass
-class LATimesConfig:
-    output_folder = os.path.join(os.getcwd(), 'output')
-    excel_file = os.path.join(output_folder, 'news.xlsx')
-    image_folder = os.path.join(output_folder, 'images')
